63eca010406802bab49a39198f0d3c466d45f63f
Timothy Escopete #local #osxy <me@timothyae.com>
2015-04-15 13:34:10 +0800

    Update install.sh;

--- install.sh
+++ install.sh
@@ -74,7 +74,7 @@ fi
 text=`echo "$1" | sed "s: :%20:g"`
 
 # Generate commits using 3rd party service
-curl "http://pokrovsky.herokuapp.com/$user/.repo/$text" | sed -e "s/git init .repo/echo; echo; echo -e \"Generating commits using service from pokrovsky.herokuapp.com.\"; echo; git init .repo; echo;/g" | sed -e "/git pull/d" | sed -e "s/.repo.git/$repo.git/g" | bash
+curl "http://pokrovsky.herokuapp.com/$user/.repo/$text" | sed -e "s/git init .repo/echo \"\"; echo \"\"; echo -e \"Generating commits using service from pokrovsky.herokuapp.com.\"; echo \"\"; git init .repo; echo \"\";/g" | sed -e "/git pull/d" | sed -e "s/.repo.git/$repo.git/g" | bash
 
 # Delete .repo dir
 rm -rf .repo
